0e14a1d120084c0bb147797abe97a191c5041306
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2025-10-02 14:28:12 +0100

    made news letter section responsive

--- components/news-letter.tsx
+++ components/news-letter.tsx
@@ -7,9 +7,9 @@ import { icons } from "@/constants/icons";
 
 export default function NewsLetter() {
   return (
-    <div className=" custom-padding bg-[#C9E6FE] my-[3rem] flex justify-between items-center py-[3.2rem]">
+    <div className=" custom-padding bg-[#C9E6FE] my-[3rem] flex flex-col md:flex-row justify-between gap-4 md:gap-0 items-center py-[3.2rem]">
       <div className=" flex flex-col gap-4">
-        <h3 className=" font-[650] text-4xl text-foreground ">
+        <h3 className=" font-[650] text-center md:text-left text-4xl text-foreground ">
           Join our Newsletter
         </h3>
         <p className=" text-xl">
@@ -20,15 +20,15 @@ export default function NewsLetter() {
       <div className=" flex items-center gap-2">
         <Input
           placeholder="enter your email address"
-          className="rounded-[15px] border-[1px] border-muted w-[30rem] bg-transparent py-6 z-10"
+          className="rounded-[15px] border-[1px] border-muted md:w-[30rem] bg-transparent py-6 z-10"
         />
-        <button className="bg-primary text-background px-5 font-bold py-3 rounded-xl ">
+        <button className="bg-primary text-background px-5 w-48 md:w-auto font-bold py-3 rounded-xl ">
           Learn More
         </button>
       </div>
 
       <Image
-        className="absolute left-[44.5%] bottom-[13%] object-cover"
+        className="absolute left-[44.5%] rotate-y-180 md:rotate-y-0 bottom-[13%] object-cover"
         src={icons.kiteIcon}
         width={170}
         height={170}
